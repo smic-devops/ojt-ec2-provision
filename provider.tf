663f2bb50d31f52e765c9c8ce37ae31bc3b4816b
OJT David Aldwin De Guzman <ojt.ddeguzman@smic.sm.ph>
2025-02-20 17:12:29 +0800

    Terraform files

---
+++ provider.tf
@@ -0,0 +1,20 @@
+# provider.tf
+terraform {
+  required_version = ">= 1.6"
+
+
+  required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = ">= 5.0"
+   }
+  }
+}
+
+
+provider "aws" {
+  region     = "ap-southeast-1"
+  access_key = "AWS access key"
+  secret_key = "AWS secret key"
+  token      = "AWS token"
+}
