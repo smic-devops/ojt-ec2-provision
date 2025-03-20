b99129208f463ec6a597ab920ec3bacdcd6bb2f3
OJT David Aldwin De Guzman <ojt.ddeguzman@smic.sm.ph>
2025-03-20 10:09:31 +0800

    Update main

--- main.tf
+++ main.tf
@@ -94,10 +94,33 @@ resource "aws_lb_target_group" "tg" {
 }
 
 # ALB HTTP Listener
-resource "aws_lb_listener" "listener" {
+resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      protocol = "HTTPS"
+      port     = "443"
+      status_code = "HTTP_301"
+    }
+  }
+
+  tags = {
+    Environment    = "Sandbox"
+    Resource_Types = "Instances Volumes Network_Interfaces"
+  }
+}
+
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
 
   default_action {
     type             = "forward"
